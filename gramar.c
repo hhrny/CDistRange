6e20b7e3eedca44ababadc2f44e4268914a59188
hhrny <1016755439@qq.com>
2016-11-16 17:07:04 +0800

    add pointer, struct and array

--- gramar.c
+++ gramar.c
@@ -1,23 +1,49 @@
 #include <stdio.h>
 
+struct A{
+    int num;
+    char data[8];
+    struct A *next;
+}
+
 int fun(char c){
     int tmp = (int)c;
     int tmp2 = tmp + tmp;
     return tmp2/tmp+1-3*(-tmp);
 }
 
+void fun2(struct A * a){
+    (*a).num = 1;
+    a->data[0] = 'a';
+    a->next = 0;
+}
+
+void printA(struct A a){
+    int n = 0;
+    print_i(a.num);
+    while(n<a.num){
+        print_c(a.data[n]);
+        n++;
+    }
+}
+
 int main(){
+    struct A array[10];
     int n = 0;
     char c = 'a';
-    while(n++<10){
+    while(n<10){
+        array[n].num = 0;
+        array[n].next = 0;
+        n ++;
         if(fun(c) > 10){
-            printf("hello ");
+            print_i(n);
         }
         else{
-            printf("world !\n");
+            print_c(c);
         }
-        c ++;
     }
-    printf("\n");
+    fun2(&array[0]);
+    printA(array[0]);
+    print_c('n');
     return 0;
 }
